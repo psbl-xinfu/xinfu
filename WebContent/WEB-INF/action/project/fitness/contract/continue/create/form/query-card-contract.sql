9b4a22db1b747f7ca4aff1fbc79538523e168454
zhaoyabin <zhaoyabin@gymjam.cn>
2018-12-12 18:31:54 +0800

    多次续卡

--- WebContent/WEB-INF/action/project/fitness/contract/continue/create/form/query-card-contract.sql
+++ WebContent/WEB-INF/action/project/fitness/contract/continue/create/form/query-card-contract.sql
@@ -1,6 +1,7 @@
 select 
 	d.code as cardcode
-	,d.contractcode 
+	,d.contractcode,
+	e.type
 from cc_card d 
 inner join cc_contract t on t.code = d.contractcode and t.org_id = d.org_id 
 inner join cc_customer c on d.customercode = c.code and c.org_id = d.org_id 
@@ -14,10 +15,12 @@ and d.status != 0 and d.status != 6 and t.status >= 2
 and t.normalmoney = COALESCE(t.factmoney,0.00) + COALESCE((
 	select sum(COALESCE(t2.factmoney,0.00)) from cc_contract t2 
 	where t2.relatecode = t.code and t2.org_id = t.org_id and t2.status >= 2
-),0.00) 
+),0.00)
+--去掉里面的话就是多次续卡
 --and not exists(
 --	select 1 from cc_card d2 
 --	where d2.code = d.code and d2.org_id = d.org_id and d2.isgoon = 1 and d2.status != 0 and d2.status != 6 
 --)
+--
 and e.maxusernum = 1
 and (d.relatecode is null or d.relatecode='') --排除副卡
--- WebContent/WEB-INF/action/project/fitness/contract/continue/create/form/query-starttype.sql
+++ WebContent/WEB-INF/action/project/fitness/contract/continue/create/form/query-starttype.sql
@@ -2,4 +2,4 @@ select
 	domain_value,
 	domain_text_en as domain_text_cn 
 from t_domain 
-where namespace = 'StartType'
+where namespace = 'StartType'  and domain_value='0'
