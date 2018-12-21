9b5048c50c9bb6223bf9bb9d43a79fc1c170f8e9
zhaoyabin <zhaoyabin@gymjam.cn>
2018-12-21 09:59:02 +0800

    单次消费的恢复

--- WebContent/WEB-INF/action/project/fitness/finance/singleitem/form/query-customer.sql
+++ WebContent/WEB-INF/action/project/fitness/finance/singleitem/form/query-customer.sql
@@ -1,19 +1,8 @@
 select 
-	code as cust_code,
 	name as cust_name,
 	mobile,
 	moneygift,
 	moneycash
 from
-	cc_customer where 
-mobile = ${fld:mobile} and org_id = ${def:org}
-UNION
-select 
-	code as cust_code,
-	name as cust_name,
-	mobile,
-	null as moneygift,
-	null as moneycash
-from
-	cc_guest where 
-mobile = ${fld:mobile} and org_id = ${def:org} ORDER BY moneygift,moneycash desc  limit 1
+	cc_customer
+where code = ${fld:custcode} and org_id = ${def:org}
--- WebContent/WEB-INF/action/project/fitness/finance/singleitem/search/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/finance/singleitem/search/query-base.sql
@@ -5,28 +5,16 @@ select
     (case when s.isguazhang=1 then '挂账' else (
     	case when s.paytype=1 then '现金/银行卡支付' else '储蓄卡支付' end
     ) end) as payment,
-    (case when (select c.mobile from cc_customer c where c.mobile =
-  ${fld:mobile}) is null then g.code 
-  else cust.code  end ) as code,
-   (case when (select c.mobile from cc_customer c where c.mobile =
-  ${fld:mobile}) is null then g.mobile 
-  else cust.mobile  end ) as mobile,
-  (case when (select c.mobile from cc_customer c where c.mobile =
-  ${fld:mobile}) is null then g.name 
-  else cust.name  end ) as cust_name,
+    cust.code,
+    cust.name as cust_name,
     (case when s.status=1 then '未付款' else '已付款' end) as status,
     (select name from hr_staff where userlogin = s.createdby) as createdby,
     s.created
 from cc_singleitem s
 left join cc_customer cust on cust.code=s.customercode and s.org_id=cust.org_id
-left join cc_guest g on g.code=s.customercode and s.org_id=g.org_id
-
 where
   1=1 
-  and 
-  (case when (select c.mobile from cc_customer c where c.mobile =
-  ${fld:mobile}) is null then g.mobile = ${fld:mobile} 
-  else cust.mobile = ${fld:mobile}  end )
+  and cust.code = ${fld:custcode}
   and s.org_id = ${def:org}
 ${filter} 
 ${orderby}
