3b08be9dfb936133185276246b3b79c8571e62f1
zhaoyabin <zhaoyabin@gymjam.cn>
2018-12-28 14:53:43 +0800

    预定时间设置

--- src/transactions/project/fitness/job/sql/site/update.sql
+++ src/transactions/project/fitness/job/sql/site/update.sql
@@ -2,5 +2,5 @@ UPDATE cc_siteusedetail
 SET status=0,
 	remark=concat(remark, '，系统提示：该预约已超过系统预约时间，自动取消！')
 where prepare_date+prepare_starttime+ INTERVAL ${fld:sitedate_value} <= '${def:timestamp}'::timestamp 
-and paystatus=0 and status!=2
+and paystatus=0 and status!=2 and status!=3
 and org_id=${fld:org_id}
