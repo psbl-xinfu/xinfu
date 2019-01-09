1a160606408cef169c514023b201c3713e1c2d25
zhaoyabin <zhaoyabin@gymjam.cn>
2019-01-09 08:47:46 +0800

    场地定时器的修改

--- src/transactions/project/fitness/job/sql/site/update.sql
+++ src/transactions/project/fitness/job/sql/site/update.sql
@@ -1,6 +1,6 @@
 UPDATE cc_siteusedetail 
 SET status=0,
 	remark='系统提示：该预约已超过系统预约时间，自动取消！'
-where prepare_date+prepare_starttime+ INTERVAL ${fld:sitedate_value} <= '${def:timestamp}'::timestamp 
+where  '${def:timestamp}'::timestamp >=(prepare_date+prepare_starttime + INTERVAL ${fld:sitedate_value})
 and paystatus=0 and status='1'
 and org_id=${fld:org_id}
