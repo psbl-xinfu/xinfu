ae18a7c35aa9f9c4084d11772802fc3cc5117779
zhaoyabin <zhaoyabin@gymjam.cn>
2019-02-14 15:32:28 +0800

    赠课预约的修改

--- WebContent/WEB-INF/action/project/fitness/class/private/privateteach/covenantclass/insert/insert.sql
+++ WebContent/WEB-INF/action/project/fitness/class/private/privateteach/covenantclass/insert/insert.sql
@@ -14,20 +14,16 @@ INSERT INTO cc_ptprepare(
 ) 
 select 
 	${seq:nextval@seq_cc_ptprepare},
-	(
-		case when pttype != 5 then ptid else (
-			select c.pt from cc_customer c where c.code = p.customercode and c.org_id = p.org_id 
-		) end
-	),
-	code,
-	customercode,
+	p.ptid,
+	p.code,
+	p.customercode,
 	${fld:pdate},
 	(${fld:hour} ||':'||${fld:minute})::time,
 	1,
 	'${def:user}',
 	{ts '${def:timestamp}'},
 	(${fld:hour} ||':'||${fld:minute})::time,
-	(select ((${fld:hour} ||':'||${fld:minute})::time+ (times||' minutes')::interval) from cc_ptdef where code = ptlevelcode),
+	(select ((${fld:hour} ||':'||${fld:minute})::time+ (times||' minutes')::interval) from cc_ptdef where code = p.ptlevelcode),
 	${def:org} 
 from cc_ptrest p 
-where code = ${fld:ptcode} and org_id=${def:org}
+where p.code = ${fld:ptcode} and p.org_id=${def:org}
