74052ee92d0ae3ed63352a544624b27d2068c99e
Leo <leo@cchup.com>
2019-02-28 10:29:16 +0800

    修正Bug 121 115 微信私教分析页跳转主页问题，私教我的会员、潜在资源筛选查询结果出现空白问题。

--- WebContent/WEB-INF/action/project/fitness/wx/pt/myziyuan/search/script.js
+++ WebContent/WEB-INF/action/project/fitness/wx/pt/myziyuan/search/script.js
@@ -1,3 +1,5 @@
+// modified by leo 190228修正我的资源筛选空白问题
+//$('.list_a').remove();
 $('.list_a').remove();
 <rows>
 appendCust('${fld:code}','${fld:name}','${fld:sex}','${fld:mobile}','${fld:lasttime}','${fld:headpic}');
