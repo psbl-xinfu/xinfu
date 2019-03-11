eecf2d5c4365adae4ebc074b7f3174c1d36251ae
zhaoyabin <zhaoyabin@gymjam.cn>
2019-03-11 20:44:51 +0800

    合同的修改

--- WebContent/WEB-INF/action/project/fitness/contract/contractlist/cardlist/search/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/contract/contractlist/cardlist/search/query-base.sql
@@ -13,7 +13,7 @@ from
 	(case when ${fld:contracttype}='1' and isaudit=1 then '未审批' when ${fld:contracttype}='1' and isaudit=3 then '审批拒绝' 
  	when ${fld:contracttype}='1' and c.status = 1 then '未付款' when ${fld:contracttype}='1' and c.status =2 then '已付款'
  	when ${fld:contracttype}='2' then (case when c.normalmoney = (select (ct.factmoney+c.factmoney) from cc_contract ct 
-			where ct.relatecode = c.code and ct.org_id = c.org_id) then '已还款' else '未还款' end)
+			where ct.relatecode = c.code and ct.org_id = c.org_id and ct.status =2 and ct.type!=2 ) then '已还款' else '未还款' end)
 	
 	when ${fld:contracttype}='3' and COALESCE(c.normalmoney, 0) != COALESCE(c.factmoney, 0) then '未付清'
 	when ${fld:contracttype}='3' and COALESCE(c.normalmoney, 0) = COALESCE(c.factmoney, 0) then '已付清'
