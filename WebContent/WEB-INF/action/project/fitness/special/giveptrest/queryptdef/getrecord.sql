e0a77ce07e19a843bfcfe3129ff9a6f75a7aba98
zhaoyabin <zhaoyabin@gymjam.cn>
2019-03-12 17:11:57 +0800

    曾课的修改

--- WebContent/WEB-INF/action/project/fitness/special/giveptrest/queryptdef/getrecord.sql
+++ WebContent/WEB-INF/action/project/fitness/special/giveptrest/queryptdef/getrecord.sql
@@ -6,3 +6,7 @@ FROM cc_ptdef pd
 left join cc_ptdef_limit pl on pd.code= pl.ptdefcode and pd.org_id = pl.org_id
 where pd.org_id = ${def:org}
 and pl.pt = ${fld:pt}
+and (case when (select distinct pt from cc_customer where code=${fld:cust} and org_id=${def:org})!=pl.pt then pd.reatetype=0
+when (select distinct pt from cc_customer where code=${fld:cust} and org_id=${def:org})=pl.pt then 1=1  else
+1=1
+ end)
