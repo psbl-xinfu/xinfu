123b50f97d3c8cb055062046b4f2763d9147783d
zhaoyabin <zhaoyabin@gymjam.cn>
2019-03-13 10:31:09 +0800

    体验课只能有一张和头部私教查询和私教剩余课时查询修改

--- WebContent/WEB-INF/action/project/fitness/guest/detail/gustmenu/reservationdetail/queryptrest/query-ptrest.sql
+++ WebContent/WEB-INF/action/project/fitness/guest/detail/gustmenu/reservationdetail/queryptrest/query-ptrest.sql
@@ -1,18 +1,13 @@
 select 
-	pttotalcount::int,
-	ptleftcount::int,
-	(
-		case when cc_ptrest.pttype = 5 then (
-			select staff.name from hr_staff staff 
-			inner join cc_customer cust on staff.userlogin = cust.pt 
-			where cust.code = cc_ptrest.customercode and cust.org_id = cc_ptrest.org_id 
-		)
-		else (select staff.name from hr_staff staff where staff.userlogin = cc_ptrest.ptid) end
-	) as name,
-	(select ptlevelname from cc_ptdef where code = ptlevelcode 
-		and org_id = ${def:org}) as ptlevelname,
-	(select count(1) from cc_ptlog where ptrestcode = cc_ptrest.code 
+	pr.pttotalcount::int,
+	pr.ptleftcount::int,
+(select name from hr_staff where userlogin = 
+(case when pt.reatetype=1 then cust.pt else pr.ptid end)  ) as name,
+	pt.ptlevelname,
+	(select count(1) from cc_ptlog where ptrestcode = pr.code 
 		and customercode = ${fld:custcode} and org_id = ${def:org}) as num
-from cc_ptrest
-where pttype in (1,3,4,5) and (ptenddate::date>='${def:date}'::date or ptenddate is null)
-and org_id = ${def:org} and customercode = ${fld:custcode}
+from cc_ptrest pr
+left join cc_customer cust on pr.customercode = cust.code and pr.org_id = cust.org_id
+left join cc_ptdef pt on pt.code = pr.ptlevelcode and pt.org_id = pr.org_id
+where pr.pttype in (1,3,4,5) and (pr.ptenddate::date>='${def:date}'::date or pr.ptenddate is null)
+and pr.org_id = ${def:org} and pr.customercode = ${fld:custcode}
