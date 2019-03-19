23c963798f649ee6d5dca43c73630306e321dfb6
defuxiaozhu <defuxiaozhu@192.168.3.112>
2019-03-19 18:52:21 +0800

    1、修改来访预约页面添加资源，在会籍手机端办理不显示 来访预约页面添加资源 2、新增陌生来访指定接待会籍后来访列表显示不正确

--- WebContent/WEB-INF/action/project/fitness/guest/visitlist/insert/insert-guest.sql
+++ WebContent/WEB-INF/action/project/fitness/guest/visitlist/insert/insert-guest.sql
@@ -5,6 +5,7 @@ insert into cc_guest
 	sex,
 	name,
 	mobile,
+	age, --zzn190319
 	org_id,
 	created,
 	mc
@@ -16,6 +17,7 @@ insert into cc_guest
 		${fld:sex},
 		${fld:guest},
 		${fld:mobile},
+		0,--zzn190319
 		${def:org},
 		{ts '${def:timestamp}'},
 		${fld:cc_mc}
