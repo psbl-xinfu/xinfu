ba62326bbc06475731c555b6449803c638b0ab3a
zhaoyabin <zhaoyabin@gymjam.cn>
2019-03-21 11:12:52 +0800

    租柜的修改

--- WebContent/WEB-INF/action/project/fitness/contract/cabinet/create/cabinetcode/script.js
+++ WebContent/WEB-INF/action/project/fitness/contract/cabinet/create/cabinetcode/script.js
@@ -2,7 +2,8 @@ var ptstr = "<option value=''>请选择</option>";
 <cabinetcode-rows>
 	ptstr+="<option code='${fld:cabinetcode@js}' price='${fld:price}' value='${fld:tuid}'>${fld:cabinetcode@js}</option>"
 </cabinetcode-rows>
-$("#cabinet_code_id").html(ptstr);
-
-$("#cabinet_code_id").selectpicker("refresh");
-$("#cabinet_code_id").selectpicker("render");
+//修改select id号  zyb 2019-3-21
+$("#cabinetid").html(ptstr);
+//修改id号  zyb 2019-3-21
+$("#cabinetid").selectpicker("refresh");
+$("#cabinetid").selectpicker("render");
