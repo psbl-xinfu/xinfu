e878358fda403d20856a8f9d9bed37375b33829c
zhaoyabin <zhaoyabin@gymjam.cn>
2019-03-25 09:44:10 +0800

    微信新增预约的修改

--- WebContent/WEB-INF/action/project/fitness/wx/pt/mycust/addyuyue/crud/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/wx/pt/mycust/addyuyue/crud/query-base.sql
@@ -1,10 +1,10 @@
-SELECT  ptdef.code,
-        ptdef.ptlevelname
+SELECT  ptdef.code as ptcode,
+        ptdef.ptlevelname,
+        ptrest.ptleftcount
  FROM cc_ptrest ptrest
     LEFT JOIN cc_ptdef ptdef ON ptdef.code=ptrest.ptlevelcode and ptdef.org_id = ptrest.org_id 
  WHERE ptrest.customercode=${fld:customercode} and ptrest.org_id=${def:org} and ptdef.status=1
-       and ptrest.ptid='${def:user}'
-     
+      and (case when ptdef.reatetype=1 then 1=1 else ptrest.ptid='${def:user}' end)
     
     
 	
