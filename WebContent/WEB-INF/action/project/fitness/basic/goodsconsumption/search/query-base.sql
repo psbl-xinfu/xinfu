f6b350cdf88ca8a91810c1782bb06e64d1b41126
defuxiaozhu <defuxiaozhu@192.168.3.112>
2019-03-27 11:27:55 +0800

    商品销售：操作员可以是其他店员工；card只取本店员工

--- WebContent/WEB-INF/action/project/fitness/basic/goodsconsumption/search/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/basic/goodsconsumption/search/query-base.sql
@@ -6,12 +6,12 @@ select
 	card.code as cardcode,
 	ls.factmoney,
 	ls.created,
-	(select name from hr_staff where userlogin = ls.createdby and org_id = ${def:org}) as staff_name,
+	(select name from hr_staff where userlogin = ls.createdby ) as staff_name,  --zzn20190327 其他店的管理员也可能进店销售
 	(case when ls.paystatus=1 then '未付款' when ls.paystatus=2 then '已付款' end) as paystatus,
 	(select sum(amount) from cc_leave_stock_goods where leave_stock_id = ls.tuid and org_id = ${def:org})::int as amount 
 from cc_leave_stock ls
 left join cc_customer cust on ls.customercode = cust.code and ls.org_id = cust.org_id
-left join cc_card card on ls.paycardcode = card.code and card.isgoon = 0
+left join cc_card card on ls.paycardcode = card.code and card.isgoon = 0 and card.org_id=ls.org_id  --zzn20190327
 
 where ls.org_id = ${def:org} and ls.status=2
 
