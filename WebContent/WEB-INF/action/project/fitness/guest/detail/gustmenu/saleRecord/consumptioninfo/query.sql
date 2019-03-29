eb00118715f6d4a9cc3c1e0a5c319282c9eb7339
defuxiaozhu <defuxiaozhu@192.168.3.112>
2019-03-29 18:08:01 +0800

    修改会员信息汇总中的消费信息，增加商品销售的记录

--- WebContent/WEB-INF/action/project/fitness/guest/detail/gustmenu/saleRecord/consumptioninfo/query.sql
+++ WebContent/WEB-INF/action/project/fitness/guest/detail/gustmenu/saleRecord/consumptioninfo/query.sql
@@ -1,7 +1,7 @@
 select
-	config.param_value,
-	config.param_text,
-	sum(c.factmoney) as normalmoney
+	config.param_value as param_value,
+	config.param_text as param_text,
+	sum(c.factmoney)::numeric(10,2) as normalmoney
 from cc_config config
 inner join cc_operatelog op on op.opertype::int=config.param_value::int
 inner join cc_contract c on op.pk_value = c.code and op.org_id = c.org_id
@@ -9,6 +9,21 @@ where op.customercode=${fld:id} and op.org_id='${def:org}' and config.category =
 and config.org_id = (case when 
 	not exists(select 1 from cc_config c where c.org_id =${def:org} and c.category=config.category) 
 	then (select org_id from hr_org where pid is null or pid = 0) else ${def:org} end)
-GROUP BY config.param_value,config.param_text
-order by sum(c.factmoney) desc
+GROUP BY param_value,param_text
+--zzn 2019-03-29 union商品销售记录
+union
+
+select
+	config.param_value as param_value,
+	config.param_text as param_text,
+	sum(op.factmoney)::numeric(10,2) as normalmoney
+from cc_config config
+inner join cc_operatelog op on op.opertype::int=config.param_value::int
+where op.customercode=${fld:id} and op.org_id='${def:org}' and config.category = 'OpeCategory'
+and config.org_id = (case when 
+	not exists(select 1 from cc_config c where c.org_id =${def:org} and c.category=config.category) 
+	then (select org_id from hr_org where pid is null or pid = 0) else ${def:org} end)
+GROUP BY param_value,param_text
+
+order by normalmoney desc
 
--- WebContent/WEB-INF/action/project/fitness/guest/detail/gustmenu/saleRecord/search/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/guest/detail/gustmenu/saleRecord/search/query-base.sql
@@ -23,7 +23,7 @@ as prepaid,--已付
 		),0.00)
 	)::numeric(10,2) AS amount_owe,--未付
 	(select name from hr_staff where userlogin=op.createdby) as vc_iuser,--操作人
-	op.createdate,
+	op.createdate as createdate,
 	(case when c.factmoney + (case when  (select DISTINCT a.factmoney * (case when  (select count(d.relatecode) as geshu from cc_contract d 
   where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
 else  (select count(d.relatecode) as geshu from cc_contract d 
@@ -35,7 +35,7 @@ else  (select count(d.relatecode) as geshu from cc_contract d
   where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
 end ) from  cc_contract a  where a.relatecode = c.code )end) 
 =c.normalmoney then '已付清' else '未付清' end) as status,
-	op.createtime
+	op.createtime as createtime
 from cc_operatelog op
 inner join cc_contract c on op.pk_value = c.code and op.org_id = c.org_id and c.contracttype!=3
 where op.customercode=${fld:id} and op.org_id='${def:org}'
@@ -43,24 +43,41 @@ and to_char(op.createdate, 'yyyy-MM')>=${fld:startdate}
 and to_char(op.createdate, 'yyyy-MM')<=${fld:enddate} 
 and (case when ${fld:paymentstatus} is null then 1=1 else 
 		(case when ${fld:paymentstatus}='1' then c.normalmoney= (c.factmoney + (case when  (select DISTINCT a.factmoney * (case when  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
+ 			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
 else  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
-end ) from  cc_contract a  where a.relatecode = c.code ) is null then 0
+ 			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
+			 end ) from  cc_contract a  where a.relatecode = c.code ) is null then 0
 else (select DISTINCT a.factmoney * (case when  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
+  			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
 else  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
-end ) from  cc_contract a  where a.relatecode = c.code )end))
-		else c.normalmoney!= (c.factmoney + (case when  (select DISTINCT a.factmoney * (case when  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
+  			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
+		     end ) from  cc_contract a  where a.relatecode = c.code )end))
+else c.normalmoney!= (c.factmoney + (case when  (select DISTINCT a.factmoney * (case when  (select count(d.relatecode) as geshu from cc_contract d 
+ 			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
 else  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
-end ) from  cc_contract a  where a.relatecode = c.code ) is null then 0
+ 			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
+			 end ) from  cc_contract a  where a.relatecode = c.code ) is null then 0
 else (select DISTINCT a.factmoney * (case when  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
+ 			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode ) is null then 0 
 else  (select count(d.relatecode) as geshu from cc_contract d 
-  where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
-end ) from  cc_contract a  where a.relatecode = c.code )end))
+ 			 where d.contracttype=3 and d.status=2 and c.code = d.relatecode GROUP BY  d.relatecode )
+			end ) from  cc_contract a  where a.relatecode = c.code )end))
 		end) end)
-order by op.createdate desc,op.createtime desc
+
+union 
+
+select 
+    (SELECT  param_text  FROM cc_config WHERE category = 'OpeCategory'
+	and param_value::int=op.opertype::int  
+	and org_id=1003) as type,
+	op.normalmoney,--应收
+	op.factmoney,--实收factmoney
+	op.factmoney as prepaid,--已付
+	(op.inimoney - op.factmoney)::numeric(10,2)   AS amount_owe,--未付
+(select name from hr_staff where userlogin=op.createdby) as vc_iuser,--操作人 
+op.createdate as createdate,
+(case when op.normalmoney = op.factmoney THEN '已付清' else '未付清' end ) as status,
+op.createtime as createtime
+from cc_operatelog op where op.customercode=${fld:id} and op.org_id='${def:org}'
+order by createdate desc,createtime desc
+
