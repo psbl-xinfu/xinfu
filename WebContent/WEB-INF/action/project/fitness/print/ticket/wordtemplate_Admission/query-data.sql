03531ff572748e8775979ba3653ec27aa58b184c
defuxiaozhu <defuxiaozhu@192.168.3.112>
2019-04-04 11:25:15 +0800

    次卡打印

--- WebContent/WEB-INF/action/project/fitness/print/ticket/wordtemplate_Admission/query-data.sql
+++ WebContent/WEB-INF/action/project/fitness/print/ticket/wordtemplate_Admission/query-data.sql
@@ -1,4 +1,4 @@
-select 
+select
 	(SELECT config.param_value FROM cc_config config WHERE config.category = 'Wish' 
 	  	and config.org_id = (case when 
 		not exists(select 1 from cc_config c where c.org_id = ${def:org} and c.category=config.category) 
@@ -10,11 +10,11 @@ select
 	inleft.nowcount as old_count,--原有次数
 	${fld:nowcount} as use_count,--扣次
 	inleft.nowcount - ${fld:nowcount}::int as new_count,--剩余次
-    (select inuser from cc_inleft where cc_inleft.cardcode=card.code and indate= '${def:date}' and org_id = ${def:org} order by intime desc limit 1) as vcuser,--操作人
-   	(select indate  from cc_inleft  where cc_inleft.cardcode=card.code  and indate= '${def:date}' and org_id = ${def:org} order by intime desc  limit 1) as pay_date,--操作时间
-    (select to_char(intime,'yyyy-hh-mm hh24:mi:ss')  from cc_inleft  where cardcode=card.code  and indate= '${def:date}' and org_id = ${def:org} order by intime desc  limit 1) as pay_time,--操作时间
-   	(SELECT org_name FROM hr_org WHERE org_id = ${def:org}) as vc_club,
-	(case cab.type when 0 then '健身' when 1 then'游泳' when 2 then '洗浴' end) as vc_type --类型
+    (select inuser from cc_inleft where cc_inleft.cardcode=card.code  and org_id = ${def:org} order by intime desc limit 1) as vcuser,--操作人
+   	(select indate  from cc_inleft  where cc_inleft.cardcode=card.code   and org_id = ${def:org} order by intime desc  limit 1) as pay_date,--操作时间
+    (select to_char(intime,'yyyy-hh-mm hh24:mi:ss')  from cc_inleft  where cardcode=card.code   and org_id = ${def:org} order by intime desc  limit 1) as pay_time,--操作时间
+   	(SELECT org_name FROM hr_org WHERE org_id = ${def:org}) as vc_club
+	--(case cab.type when 0 then '健身' when 1 then'游泳' when 2 then '洗浴' end) as vc_type --类型
 from  cc_inleft inleft
 LEFT JOIN cc_card card on inleft.cardcode=card.code and inleft.customercode = card.customercode
 LEFT JOIN cc_cabinettemp cab on cab.cardcode=card.code and cab.customercode = card.customercode
@@ -23,6 +23,5 @@ left join cc_customer cust on cust.code = inleft.customercode
 WHERE
 inleft.code=${fld:inleftcode}
   and	inleft.cardcode = ${fld:pk_value} and card.isgoon =0 and card.org_id = ${fld:org_id}
-	AND
-	inleft.indate= '${def:date}'   
+-- AND inleft.indate= '${def:date}'     ZZN2019-04-04
  order by intime  limit 1
