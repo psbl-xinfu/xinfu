a00c30d198d8c79d74c7ec7a2edd455f0a1ec2ed
zhaoyabin <zhaoyabin@gymjam.cn>
2019-04-08 10:32:10 +0800

    微信端私教添加约课

--- WebContent/WEB-INF/action/project/fitness/wx/pt/mycust/addyuyue/ptprepare/query-time.sql
+++ WebContent/WEB-INF/action/project/fitness/wx/pt/mycust/addyuyue/ptprepare/query-time.sql
@@ -1,4 +1,4 @@
-SELECT count(1) as time FROM dual 
+SELECT count(1) as timet FROM dual 
 
 WHERE to_date('${def:timestamp}','yyyy-MM-dd') > to_date(${fld:preparedate},'yyyy-MM-dd')
 
--- WebContent/WEB-INF/action/project/fitness/wx/pt/mycust/addyuyue/ptprepare/script.js
+++ WebContent/WEB-INF/action/project/fitness/wx/pt/mycust/addyuyue/ptprepare/script.js
@@ -1,37 +1,33 @@
-﻿if("${fld:dat}"==1){
+﻿﻿if("${fld:dat}"==1){
 	ccms.dialog.notice("该门店正在放假中，不能预约！",2000)
-	return false;
-}
- if("${fld:work}"==1){
+}else if("${fld:work}"==1){
 	ccms.dialog.notice("该预约不在门店上班时间内或超过门店上班时间，不能再次预约！",2000)
-	return false;
-}
- if("${fld:ptstatus}"==1){
+}else if("${fld:ptstatus}"==1){
 	ccms.dialog.notice("该课程已过期，不能预约！",2000)
-	return false;
-}
- if("${fld:num}"==1){
+}else if("${fld:num}"==1){
 	ccms.dialog.notice("该课程剩余课时不足，不能预约！",2000)
-	return false;
-}if("${fld:time}"==1){
+}else if("${fld:timet}"==1){
 	ccms.dialog.notice("预约日期必须大于等于当前日期，预约时间必须当天内！",2000)
-	return false;
-}
- if("${fld:workdate}"==1){
+}else if("${fld:workdate}"==1){
 	ccms.dialog.notice("该时间段已预约或上课中，不能再次预约！",2000)
-	return false;
-}
-if("${fld:ptprepare}"==1){
+}else if("${fld:ptprepare}"==1){
 	ccms.dialog.notice("该课程没有分配教练不能预约！",2000)
-	return false;
 }else{
-	ccms.dialog.notice("预约成功！",1000,function(){
-		location.href="${def:context}/action/project/fitness/wx/pt/mycust/sijiaohuiyuanmsg?customercode="+$('#customercode').val()+"&type=pt";
-	});
+	var preparedate="${fld:preparedate}";
+	var preparetime="${fld:preparetime}";
+	var customercode="${fld:customercode}";
+	var ptrestcode="${fld:ptrestcode}";
+    var url="${def:context}${def:actionroot}/insert?preparedate="+preparedate+"&preparetime="+preparetime+"&customercode="+customercode+"&ptrestcode="+ptrestcode;
+	ajaxCall(url,{
+		method: "post",
+		progress:false,
+		dataType: "script",
+		success: function() {
+			ccms.dialog.notice("预约成功！",2000);
+		}
+    });
+	
 }
-
-
-
 
 
 
