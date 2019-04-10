7f87245d83dec89d85cbd669d7407a1b529d83a4
defuxiaozhu <defuxiaozhu@192.168.3.101>
2019-04-10 17:44:57 +0800

    亚斌版本续卡删掉的

--- WebContent/WEB-INF/action/project/fitness/contract/continue/create/cardtype/query.sql
+++
@@ -1,6 +0,0 @@
-select 
-	ct.code as tcode
-FROM cc_cardtype ct 
-where ct.code = ${fld:ctcode}  and ct.org_id = ${def:org} 
-
-
--- WebContent/WEB-INF/action/project/fitness/contract/continue/create/cardtypelx/script.js
+++
@@ -1,7 +0,0 @@
-var ptstr="<option value=''>请选择</option>";
-<cardtype-rows>
-	ptstr+="<option value='${fld:code}' code='${fld:cardfee}' codeday='${fld:daycount}' codegiveday='${fld:giveday}' codept='${fld:ptcount}'>${fld:name@js}</option>"
-</cardtype-rows>
-$("#cardtype").html(ptstr);
-$("#cardtype").selectpicker("refresh");
-$("#cardtype").selectpicker("render");
