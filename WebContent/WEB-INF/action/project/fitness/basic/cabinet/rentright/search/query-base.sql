b9b10050c4e46bd64b8edd3c85dd2a6b239fb396
defuxiaozhu <defuxiaozhu@192.168.3.101>
2019-04-11 19:07:09 +0800

    格式调整

--- WebContent/WEB-INF/action/project/fitness/basic/cabinet/rentright/search/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/basic/cabinet/rentright/search/query-base.sql
@@ -23,10 +23,13 @@
 		 	(select c.mobile from cc_cabinet_rent r left join cc_customer c on  c.code=r.customercode 
 		 	where c.org_id=${def:org} and r.cabinetcode=b.cabinetcode::varchar order by r.created desc limit 1)
 	end)as mobile,
- 	(case when b.status=0 then '空闲' when b.status=1 and
- 		(select r.enddate from  cc_cabinet_rent r
- 		where r.cabinetcode::varchar=b.cabinetcode and r.org_id=${def:org} order by r.created desc limit 1)<{ts'${def:timestamp}'}::date 
- 	  	then '过期' when b.status=1 then '已占用' when b.status=2 then '无效'  else '其他'  end)as status
+ 	(case when b.status=0 then '空闲' 
+ 		  when b.status=1 and(select r.enddate from  cc_cabinet_rent r
+ 							where r.cabinetcode::varchar=b.cabinetcode and r.org_id=${def:org} order by r.created desc limit 1)<{ts'${def:timestamp}'}::date 
+ 	  	                  then '过期' 
+ 	  	  when b.status=1 then '已占用' 
+ 	  	  when b.status=2 then '无效' 
+ 	  	  else '其他'  end)as status
 from
 cc_cabinet b
 left join cc_cabinet_group g on g.tuid=b.groupid and g.org_id=${def:org}
