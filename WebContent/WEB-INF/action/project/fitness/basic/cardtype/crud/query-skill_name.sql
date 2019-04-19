ef576592171f84eedef1ef8dae7053345887bdf2
zhaoyabin <zhaoyabin@gymjam.cn>
2019-04-19 11:11:10 +0800

    mc的修改

--- WebContent/WEB-INF/action/project/fitness/basic/cardtype/crud/query-skill_name.sql
+++ WebContent/WEB-INF/action/project/fitness/basic/cardtype/crud/query-skill_name.sql
@@ -3,7 +3,7 @@ FROM hr_staff s
 inner join (select user_id from hr_staff_skill 
 	inner join (
 	select skill_id from hr_skill where 
-	skill_scope in ('2', '4')) skill on skill.skill_id = hr_staff_skill.skill_id
+	skill_scope in ('1', '2')) skill on skill.skill_id = hr_staff_skill.skill_id
 	) hs on hs.user_id = s.user_id
 WHERE s.is_member = 0 AND s.status = 1 
 AND s.org_id = ${def:org}
