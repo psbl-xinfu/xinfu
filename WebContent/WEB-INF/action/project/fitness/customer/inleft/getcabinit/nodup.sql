498e83350edaa58ea9a076e939a54115b5b7d311
defuxiaozhu <defuxiaozhu@192.168.3.101>
2019-04-22 14:04:53 +0800

    领取、归还手牌

--- WebContent/WEB-INF/action/project/fitness/customer/inleft/getcabinit/nodup.sql
+++
@@ -1,4 +0,0 @@
-SELECT 1 FROM cc_inleft
-WHERE code = ${fld:code}
-AND cabinettempcode !=''
- and org_id = ${def:org}
--- WebContent/WEB-INF/action/project/fitness/customer/inleft/getcabinit/noduptwo.sql
+++
@@ -1,4 +0,0 @@
-SELECT 1 FROM cc_inleft
-WHERE code = ${fld:code}
-AND lefttime is not null  --会员已离场
- and org_id = ${def:org}
