99079d1b3d7b022a846042225f215a9f1faf73b8
zhaoyabin <zhaoyabin@gymjam.cn>
2019-04-28 10:07:33 +0800

    体验课验证修改

--- WebContent/WEB-INF/action/project/fitness/class/private/ptdef/update/norep-ptdef.sql
+++ WebContent/WEB-INF/action/project/fitness/class/private/ptdef/update/norep-ptdef.sql
@@ -1,3 +1,7 @@
+--zyb 20190428 体验课验证修改
 select 1 from dual where
 (select reatetype from cc_ptdef where reatetype=${fld:reatetype} 
-and org_id=${def:org} limit 1)=1	
+and org_id=${def:org} and (case when 
+(select reatetype from cc_ptdef where code=${fld:code} and org_id=${def:org})='1' then null=null 
+else 1=1
+ end) limit 1)=1	
