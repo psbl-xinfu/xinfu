bb021e6f54e056bf0926fed9b262f3872723eedc
zhaoyabin <zhaoyabin@gymjam.cn>
2019-04-30 09:24:39 +0800

    每时场地设置修改

--- WebContent/WEB-INF/action/project/fitness/config/systemsettings/maintenance/sitetype/search/query-base.sql
+++ WebContent/WEB-INF/action/project/fitness/config/systemsettings/maintenance/sitetype/search/query-base.sql
@@ -7,6 +7,4 @@ FROM
 WHERE
 	config.category = 'sitetype'
 and 
-	config.org_id = (case when 
-	not exists(select 1 from cc_config c where c.org_id = ${def:org} and c.category=config.category) 
-	then (select org_id from hr_org where pid is null or pid = 0) else ${def:org} end)
+	config.org_id = '${def:org}'
--- WebContent/WEB-INF/action/project/fitness/site/prepare/crud/query-sitetype.sql
+++ WebContent/WEB-INF/action/project/fitness/site/prepare/crud/query-sitetype.sql
@@ -6,8 +6,6 @@ FROM
 WHERE
 	config.CATEGORY = 'sitetype'
 and 
-	config.org_id = (case when 
-	not exists(select 1 from cc_config c where c.org_id = ${def:org} and c.category=config.category) 
-	then (select org_id from hr_org where pid is null or pid = 0) else ${def:org} end)
+	config.org_id ='${def:org}'
 ORDER BY config.tuid
 
