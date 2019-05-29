63d0e3811c2ad058b310c4bcb88d6eca0c574e98
zhaoyabin <zhaoyabin@gymjam.cn>
2019-05-29 12:02:14 +0800

    升级合同带不出卡号的修改

--- WebContent/WEB-INF/action/project/fitness/contract/upgrade/create/form/query-card-contract.sql
+++ WebContent/WEB-INF/action/project/fitness/contract/upgrade/create/form/query-card-contract.sql
@@ -14,10 +14,11 @@ and d.status != 0 and d.status != 6 and t.status >= 2
 and t.normalmoney = COALESCE(t.factmoney,0.00) + COALESCE((
 	select sum(COALESCE(t2.factmoney,0.00)) from cc_contract t2 
 	where t2.relatecode = t.code and t2.org_id = t.org_id and t2.status >= 2
-),0.00) 
-and not exists(
-	select 1 from cc_card d2 
-	where d2.code = d.code and d2.org_id = d.org_id and d2.isgoon = 1 and d2.status != 0 and d2.status != 6 
-)
+),0.00)
+--zyb 190529 带不出卡号 因为先面的一段话把卡号去掉了
+--and not exists(
+--	select 1 from cc_card d2 
+--	where d2.code = d.code and d2.org_id = d.org_id and d2.isgoon = 1 and d2.status != 0 and d2.status != 6 
+--)
 and e.maxusernum = 1
 and (d.relatecode is null or d.relatecode='') --排除副卡
