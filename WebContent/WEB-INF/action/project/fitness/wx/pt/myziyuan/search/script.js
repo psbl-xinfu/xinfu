1b5bbc20b02d4ce23973db1c7e1af511e437dfb0
zhaoyabin <zhaoyabin@gymjam.cn>
2019-05-29 10:12:08 +0800

    微信端跳转其他人登录的界面修改

--- WebContent/WEB-INF/action/project/fitness/wx/pt/myziyuan/search/script.js
+++ WebContent/WEB-INF/action/project/fitness/wx/pt/myziyuan/search/script.js
@@ -11,7 +11,7 @@ function appendCust(code,name,sex,mobile,lasttime,headpic) //增加会员
 	
 	html=   "<div class='list' code="+code+">"+
     		'<img src="${def:context}'+headpic+'" class="touxiangpic fl">'+
-			"<a class='list_a' href='${def:context}/action/project/fitness/wx/pt/mycust/sijiaohuiyuanmsg?customercode="+code+"&type='>"+
+			"<a class='list_a' href='${def:context}/action/project/fitness/wx/pt/mycust/sijiaohuiyuanmsg?customercode="+code+"&type=pt'>"+
 		    "<div class='content fl'>"+
 		        "<p class='name'><label style='width:40%;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;' title='#"+name+"#'>"+name+"</label><span>"+sex+"</span><span>"+mobile+"</span></p>"+
 		        "<p class='time'>最近跟进："+lasttime+"</p>"+
