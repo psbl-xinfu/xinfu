f0ce802cb94b8ac473984c2811d7e101d113b351
zhaoyabin <zhaoyabin@gymjam.cn>
2019-05-29 10:41:43 +0800

    微信多出符号的修改

--- WebContent/WEB-INF/action/project/fitness/wx/home/query-skill.sql
+++ WebContent/WEB-INF/action/project/fitness/wx/home/query-skill.sql
@@ -15,6 +15,6 @@ SELECT
 			INNER JOIN hr_skill k ON sk.skill_id = k.skill_id 
 			WHERE sk.userlogin = '${def:user}'  AND k.org_id = ${def:org} 
 			AND (k.skill_name LIKE '%私教%' OR k.skill_name LIKE '%私人教练%' OR k.skill_name LIKE '%教练%') AND k.skill_name != '私教经理' 
-		) THEN '/action/project/fitness/wx/pt/home？' 
+		) THEN '/action/project/fitness/wx/pt/home' 
 		ELSE '/action/project/fitness/wx/mc/home' END 
 	) AS skilluri
