8be8024a912d189fc2424b1bd47521c7111ca5ff
zhaoyabin <zhaoyabin@gymjam.cn>
2019-05-30 09:58:34 +0800

    有续卡不可以转卡

--- WebContent/WEB-INF/action/project/fitness/contract/turn/querycardinfo/script.js
+++ WebContent/WEB-INF/action/project/fitness/contract/turn/querycardinfo/script.js
@@ -1,4 +1,11 @@
-﻿<card-rows>
+﻿var num1="${fld:num}";
+//zyb 20190529 续卡不可以转卡
+if(num1>0){
+	ccms.dialog.notice("有续卡不能转卡！", 2000);
+	document.getElementById("cardcode").options.selectedIndex = 0;//重置值
+	$("#cardcode").selectpicker("refresh");//刷新
+}else{
+	<card-rows>
 	$("#cardcontractcode").val("${fld:contractcode}");
 	$("#cardtypespan").text("${fld:cardtypename@js}");
 	$("#startdatespan").text("${fld:startdate}");
@@ -11,3 +18,6 @@
 	$("#starttypespan").text("${fld:starttypename@js}");
 	$("#supportorgsspan").text("${fld:supportorgs@js}");
 </card-rows>
+}
+
+
