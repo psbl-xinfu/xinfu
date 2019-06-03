72f8779f736beb8a8d6a20c775a40948d7c67151
zhaoyabin <zhaoyabin@gymjam.cn>
2019-06-03 18:00:37 +0800

    修改充值金额

--- WebContent/WEB-INF/action/project/fitness/customer/recharge/insert/update_custmoney.sql
+++ WebContent/WEB-INF/action/project/fitness/customer/recharge/insert/update_custmoney.sql
@@ -2,5 +2,5 @@ update cc_customer set
 	moneygift = (case when moneygift is null then 
 		(0+${fld:moneygift}) else (moneygift+${fld:moneygift}) end),
 	moneycash = (case when moneycash is null then 
-		(0+${fld:moneycash}) else (moneycash+${fld:moneycash}) end)
+		(0+${fld:moneycash}+${fld:moneygift}) else (moneycash+${fld:moneycash}+${fld:moneygift}) end)
 where code=${fld:cust_code} and org_id = ${def:org}
