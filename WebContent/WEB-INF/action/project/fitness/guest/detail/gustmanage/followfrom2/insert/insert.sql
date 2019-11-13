5b5ed50891bb60fce754bd6c208983708fbabac9
zhaoyabin <1285676831@qq.com>
2019-11-13 18:00:13 +0800

    修改多次点击事件

---
+++ WebContent/WEB-INF/action/project/fitness/guest/detail/gustmanage/followfrom2/insert/insert.sql
@@ -0,0 +1,24 @@
+insert into cc_comm
+(
+    code,
+    guestcode,
+    commresult,
+    thecontactcode,
+    nexttime,
+    remark,
+    createdby,
+    created,
+    org_id
+)
+values 
+(
+	${seq:nextval@seq_cc_comm},
+	${fld:ofcode},
+	${fld:the_state},
+	${fld:thecode},
+	${fld:_start_date},
+    ${fld:remark},
+    '${def:user}',
+    {ts'${def:timestamp}'},
+    ${def:org}
+)
