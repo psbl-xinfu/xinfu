7bca2875a1dbb57eb3f8584780d3f4cc72b5f5f4
zhaoyabin <1285676831@qq.com>
2019-11-18 11:42:18 +0800

    首页小数点有多位只保留两位的修改

--- WebContent/WEB-INF/action/project/fitness/home/main/leader/mcloadtarget/script.js
+++ WebContent/WEB-INF/action/project/fitness/home/main/leader/mcloadtarget/script.js
@@ -6,7 +6,7 @@ var targetfee = 0;
 </target-rows>
 
 // 本月销售业绩
-var saletotalfee = parseFloat("${fld:saletotalfee}");
+var saletotalfee = parseFloat("${fld:saletotalfee}").toFixed("2");
 var saleRate = parseFloat(saletotalfee)/parseFloat(targetfee > 0 ? targetfee : 1);
 saleRate = (saleRate > 1.00 ? 1.00 : saleRate).toFixed(2);
 var liquidFillObj = new liquidFillCharts("mcsaleDiv");
