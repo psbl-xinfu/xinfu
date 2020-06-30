ac7f2bbc2e5aa3bb00fe3a8d720cfc9c735a314f
Administrator <Administrator@XZ18H07S3XI3DLC>
2020-06-30 13:42:17 +0800

    修改资源添加不显示其他的会籍

--- WebContent/WEB-INF/action/project/fitness/guest/detail/gustmanage/crud/query-currentskill_name.sql
+++ WebContent/WEB-INF/action/project/fitness/guest/detail/gustmanage/crud/query-currentskill_name.sql
@@ -1,4 +1,10 @@
-SELECT DISTINCT s.user_id, s.userlogin, s.name, s.name_en 
-FROM hr_staff s 
-where
-s.userlogin = '${def:user}' 
+select 
+	staff.userlogin,
+	staff.name 
+from hr_staff staff
+inner join (select ss.user_id from hr_staff_skill ss
+		inner join (select skill_id from hr_skill where skill_scope in('2','4') and org_id = ${def:org}) skill
+		on skill.skill_id = ss.skill_id
+	) hs on staff.user_id = hs.user_id
+where staff.status = 1 
+and staff.org_id = ${def:org}
