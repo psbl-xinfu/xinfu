90ea756efe621b7d701f03ea820d5a66f73bb15e
Administrator <Administrator@XZ18H07S3XI3DLC>
2020-07-20 14:47:58 +0800

    添加卡内码的修改

--- WebContent/WEB-INF/action/project/fitness/basic/cardcode/insert/nodup-cardcode.sql
+++ WebContent/WEB-INF/action/project/fitness/basic/cardcode/insert/nodup-cardcode.sql
@@ -1,3 +1,3 @@
 SELECT 1 FROM cc_cardcode
 WHERE cardcode = ${fld:cardcode} 
-AND status = 1
+AND status = 1 and org_id = ${def:org}
--- WebContent/WEB-INF/action/project/fitness/basic/cardcode/insert/nodup-incode.sql
+++ WebContent/WEB-INF/action/project/fitness/basic/cardcode/insert/nodup-incode.sql
@@ -1,3 +1,3 @@
 SELECT 1 FROM cc_cardcode
 WHERE incode = ${fld:incode} 
-AND status = 1
+AND status = 1 and org_id = ${def:org}
