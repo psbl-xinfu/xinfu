348ccc422777d6c00e6d11aefe88bc9fac150eab
Administrator <Administrator@XZ18H07S3XI3DLC>
2020-12-22 14:35:54 +0800

    sdfs

--- WebContent/WEB-INF/action/project/fitness/customer/cardlist/search/clause-enddate.sql
+++ WebContent/WEB-INF/action/project/fitness/customer/cardlist/search/clause-enddate.sql
@@ -1,2 +1,3 @@
-and
+and (case ${fld:status} when '2' then 1=1 else
 	(case when ${fld:cardall} is null then card.startdate::date<=${fld:enddate} else 1=1 end)
+end)
--- WebContent/WEB-INF/action/project/fitness/customer/cardlist/search/clause-startdate.sql
+++ WebContent/WEB-INF/action/project/fitness/customer/cardlist/search/clause-startdate.sql
@@ -1,2 +1,4 @@
-and
+and (case ${fld:status} when '2' then 1=1 else
 	(case when ${fld:cardall} is null then card.startdate::date>=${fld:startdate} else 1=1 end)
+end)
+
